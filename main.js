19239af144e7674b90a43577c75f1e159a9dd3f0
HamzahZia <hamzah.uzair@hotmail.com>
2018-02-04 05:14:53 -0500

    modified algorithm

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var b = require('./meta_maker.js');
 
-b.getMetaData('ts.mp3', function (res) {
+b.getMetaData('https://listenaminute.com/o/ozone_layer.mp3', function (res) {
 		console.log(JSON.stringify(res, null, 2));
 		});
